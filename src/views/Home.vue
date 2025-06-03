08805a1c686981ce1d40f46e62bf3786d8805b19
Soburat-u-Jemeelah <soburatdamilola2024@gmail.com>
2025-06-03 18:43:34 +0100

    ADDED THE REMAINING 4 SECTIONS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,17 +8,27 @@
       <AboutSectionVue></AboutSectionVue>
       <FoodMenuSectionVue></FoodMenuSectionVue>
       <CtaVue></CtaVue>
+      <Delivery></Delivery>
+      <Testimonial></Testimonial>
+      <Banner></Banner>
+      <Blog></Blog>
     </main>
+    <Footer></Footer>
   </div>
 </template>
 
 <script setup>
 import AboutSectionVue from "../components/about/AboutSection.vue";
 import FoodMenuSectionVue from "../components/food-menu/FoodMenuSection.vue";
-import CtaVue from "../components/cta/Cta.vue";
 import headerVue from "../components/header/header.vue";
 import HeroSectionVue from "../components/hero/HeroSection.vue";
 import PromoVue from "../components/Promo/Promo.vue";
+import CtaVue from "../components/cta/Cta.vue";
+import Delivery from "../components/delivery/Delivery.vue"
+import Testimonial from "../components/testimonial/Testimonial.vue"
+import Banner from "../components/banner/Banner.vue"
+import Blog from "../components/blog/Blog.vue"
+import Footer from "../components/footer/Footer.vue"
 </script>
 
 <style scoped></style>
