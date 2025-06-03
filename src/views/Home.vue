baf1b7bb7e41de8a7e333d3017fdaee1cc04554e
TeeboiCode <oluwaleyetaiwo5228@gmail.com>
2025-06-03 15:45:01 +0100

    Added Food Menu  Section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,14 +6,22 @@
       <HeroSectionVue></HeroSectionVue>
       <PromoVue></PromoVue>
       <AboutSectionVue></AboutSectionVue>
+<<<<<<< HEAD
       <FoodMenuSectionVue></FoodMenuSectionVue>
+=======
+      <CtaVue></CtaVue>
+>>>>>>> 1b339d06cb04ec581e230b5273e8e8de902a7594
     </main>
   </div>
 </template>
 
 <script setup>
 import AboutSectionVue from "../components/about/AboutSection.vue";
+<<<<<<< HEAD
 import FoodMenuSectionVue from "../components/food-menu/FoodMenuSection.vue";
+=======
+import CtaVue from "../components/cta/Cta.vue";
+>>>>>>> 1b339d06cb04ec581e230b5273e8e8de902a7594
 import headerVue from "../components/header/header.vue";
 import HeroSectionVue from "../components/hero/HeroSection.vue";
 import PromoVue from "../components/Promo/Promo.vue";
