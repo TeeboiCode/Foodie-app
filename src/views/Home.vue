b8c9f23488a7ea68ef05faf154d1c78e0974e4a7
TeeboiCode <oluwaleyetaiwo5228@gmail.com>
2025-06-03 13:47:19 +0100

    second commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="home container-fluid p-0">
     <headerVue></headerVue>
-    <HeroSectionVue></HeroSectionVue>
-    <main></main>
+
+    <main>
+      <HeroSectionVue></HeroSectionVue>
+      <PromoVue></PromoVue>
+    </main>
   </div>
 </template>
 
 <script setup>
 import headerVue from "../components/header/header.vue";
 import HeroSectionVue from "../components/hero/HeroSection.vue";
+import PromoVue from "../components/Promo/Promo.vue";
 </script>
 
 <style scoped></style>
