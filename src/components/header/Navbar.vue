86d45a382c1f8052ac4ae3711d3d113a0ca1df02
TeeboiCode <oluwaleyetaiwo5228@gmail.com>
2025-06-11 14:07:14 +0100

    Updated the header scss and Navbar component

--- src/components/header/Navbar.vue
+++ src/components/header/Navbar.vue
@@ -5,10 +5,32 @@
         <router-link to="/" class="navbar-link" data-nav-link>Home</router-link>
       </li>
 
-      <li class="nav-item">
-        <router-link to="/about" class="navbar-link" data-nav-link
-          >About Us</router-link
+      <li class="nav-item dropdown">
+        <a
+          class="navbar-link dropdown-toggle"
+          href="#"
+          role="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
         >
+          Pages
+        </a>
+        <ul class="dropdown-menu border-0 rounded-0">
+          <li>
+            <router-link
+              to="/about"
+              class="dropdown-item border-bottom"
+              data-nav-link
+              >About Us</router-link
+            >
+          </li>
+
+          <li>
+            <a class="dropdown-item border-bottom" href="#">Another action</a>
+          </li>
+
+          <li><a class="dropdown-item" href="#">Something else here</a></li>
+        </ul>
       </li>
 
       <li class="nav-item">
@@ -28,4 +50,9 @@
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+// .nav-item.dropdown:hover .dropdown-menu {
+//   display: block;
+//   margin-top: 0; // Optional: removes the slight gap
+// }
+</style>
