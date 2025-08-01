de063bd1fd175f5bd6fbc7afaa45d68ddef1ac5e
Ayomikun <ayomikunglory02@gmail.com>
2025-08-01 16:08:02 +0100

    ayomikun

--- src/components/TasksFolder/Ayomikun02.vue
+++ src/components/TasksFolder/Ayomikun02.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
-    <h3>Ayomikun Task 02</h3>
+  
+    <div class="container py-5">
+    <div class="card mx-auto text-center border-0 shadow-sm" style="max-width: 300px; background-color: #FFF7EB; border-radius: 6px;">
+      <div class="card-body">
+        <div class="rounded-circle overflow-hidden mx-auto mb-3" style="width: 250px; height: 250px;">
+          <img src="/bootstrap/team1.png" alt="Chef" class="img-fluid">
+        </div>
+        <h5 class="card-title fw-bold">Emilia Clarke</h5>
+        <p class="card-text text-muted mb-3">Chief chef</p>
+        <div class="d-flex justify-content-center gap-3">
+          <a href="#"  class="btn btn-light rounded-circle" style="color: blue;"><i class="fa-brands fa-facebook"></i></a>
+          <a href="#" class="btn btn-light rounded-circle" style="color: rgb(14, 14, 218);"><i class="fa-brands fa-twitter"></i></a>
+          <a href="#" class="btn btn-light rounded-circle" style="color: rgb(65, 65, 143);"><i class="fa-brands fa-linkedin-in"></i></a>
+          <a href="#" class="btn btn-light rounded-circle" style="color:#d4272c"><i class="fa-brands fa-instagram"></i></a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
